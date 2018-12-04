80311cc4b45a5daff995bacbac747e360a07c5ae
frantisekspurny <frantisek@smartlook.com>
2018-12-04 14:28:34 +0100

    Changed package name

---
+++ app/src/main/aidl/com/naman14/timbersmartlook/helpers/MusicPlaybackTrack.aidl
@@ -0,0 +1,3 @@
+package com.naman14.timbersmartlook.helpers;
+
+parcelable MusicPlaybackTrack;
---
+++ app/src/main/java/com/naman14/timbersmartlook/lastfmapi/models/ScrobbleInfo.java
@@ -0,0 +1,7 @@
+package com.naman14.timbersmartlook.lastfmapi.models;
+
+/**
+ * Created by christoph on 17.07.16.
+ */
+public class ScrobbleInfo {
+}
